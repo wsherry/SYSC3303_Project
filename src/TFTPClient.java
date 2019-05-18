cb26b7f221339adb02a2993ed958a7a7fddbcd07
Alan <alanlin@cmail.carleton.ca>
2019-05-18 10:34:50 -0400

    Re-implemented server shut down for UI

--- src/TFTPClient.java
+++ src/TFTPClient.java
@@ -16,6 +16,7 @@ public class TFTPClient {
    private static String ipAddress = "";
    private static String clientDirectory = "";
    private static boolean fisnishedRequest = false;
+   private boolean running = true;
    
    // we can run in normal (send directly to server) or test
    // (send to simulator) mode
@@ -44,179 +45,185 @@ public class TFTPClient {
 	   
 	   byte readWrite = (byte) 1; //default value for initialization
 	   
-	   //After a request has been completed the user gets prompted
-	   //to enter the configuration "menu" again
-       //TO BE IMPLEMENTED AFTER FILE TRANSFER IMPLEMENTATION IS COMPLETED
-	   if (fisnishedRequest) { //fisnishedRequest should be true after a file has been fully read or written
-		   System.out.println("Enter 1 to change configerations or nothing to leave configs unchanged: ");
-		   while (!(input.equals("1") || input.equals(""))){
-			   input = sc.nextLine();
-			   if (input.equals("1")) configClient();
+	   while (running) {
+		   //After a request has been completed the user gets prompted
+		   //to enter the configuration "menu" again
+	       //TO BE IMPLEMENTED AFTER FILE TRANSFER IMPLEMENTATION IS COMPLETED
+		   if (fisnishedRequest) { //fisnishedRequest should be true after a file has been fully read or written
+			   System.out.println("Enter 1 to change configerations or nothing to leave configs unchanged: ");
+			   while (!(input.equals("1") || input.equals(""))){
+				   input = sc.nextLine();
+				   if (input.equals("1")) configClient();
+			   }
+			   fisnishedRequest = false; //set fisnishedRequest to false so the user only gets prompt after a request finishes
 		   }
-		   fisnishedRequest = false; //set fisnishedRequest to false so the user only gets prompt after a request finishes
+		   
+		   //NOT CORRECTLY IMPLEMENTED YET.
+		   //THIS SHOULD ONLY PROMPT USER AFTER A REQUEST HAS FULLY COMPLETED
+		   //User chooses read or write request
+		   while (!(input.equals("1") || input.equals("2") || input.equals("3"))){
+			   System.out.println("\nEnter '1' for read or '2' write request or '3' to quit: ");
+			   input = sc.nextLine();
+
+			   if (input.equals("1")) readWrite = (byte) 1;
+			   else if (input.equals("2")) readWrite = (byte) 2;
+			   else if (input.equals("3")) running = false;
+			   else System.out.print(input + " is not 1 or 2 or .\n");
+		   }
+		   
+		   if (!running) break; // break out of loop if user choose to quit
+		   //INCOMPLETED, need to implement error detection for checking if file exists
+		   input = " ";
+		   String fileName = "";
+		   //while (found){ //keep asking for file until valid file is found in directory
+			   System.out.println("\nEnter the name of the file: ");
+			   fileName = sc.nextLine();
+			   //CHECK IF FILE EXISTS
+		   //}
+			   
+		   byte[] msg = new byte[100], // message we send
+	             fn, // filename as an array of bytes
+	             md, // mode as an array of bytes
+	             data; // reply as array of bytes
+		   String mode; // filename and mode as Strings
+		   int j, len, sendPort;
+	      
+	      // In the assignment, students are told to send to 23, so just:
+	      // sendPort = 23; 
+	      // is needed.
+	      // However, in the project, the following will be useful, except
+	      // that test vs. normal will be entered by the user.
+		   
+	      if (run==Mode.NORMAL) 
+	         sendPort = 69;
+	      else
+	         sendPort = 23;
+	      System.out.println("Client: creating packet.");
+	         
+	      // Prepare a DatagramPacket and send it via sendReceiveSocket
+	      // to sendPort on the destination host (also on this machine).
+
+	      // if i even (2,4,6,8,10), it's a read; otherwise a write
+	      // (1,3,5,7,9) opcode for read is 01, and for write 02
+	      // And #11 is invalid (opcode 07 here -- could be anything)
+
+	      msg[0] = 0;
+	      msg[1]=readWrite;
+
+	      // convert to bytes
+	      fn = fileName.getBytes();
+	      
+	      // and copy into the msg
+	      System.arraycopy(fn,0,msg,2,fn.length);
+	      // format is: source array, source index, dest array,
+	      // dest index, # array elements to copy
+	      // i.e. copy fn from 0 to fn.length to msg, starting at
+	      // index 2
+	        
+	      // now add a 0 byte
+	      msg[fn.length+2] = 0;
+
+	      // now add "octet" (or "netascii")
+	      mode = "octet";
+	      // convert to bytes
+	      md = mode.getBytes();
+	      
+	      // and copy into the msg
+	      System.arraycopy(md,0,msg,fn.length+3,md.length);
+	      
+	      len = fn.length+md.length+4; // length of the message
+	      // length of filename + length of mode + opcode (2) + two 0s (2)
+	      // second 0 to be added next:
+
+	      // end with another 0 byte 
+	      msg[len-1] = 0;
+
+	      // Construct a datagram packet that is to be sent to a specified port
+	      // on a specified host.
+	      // The arguments are:
+	      //  msg - the message contained in the packet (the byte array)
+	      //  the length we care about - k+1
+	      //  InetAddress.getLocalHost() - the Internet address of the
+	      //     destination host.
+	      //     In this example, we want the destination to be the same as
+	      //     the source (i.e., we want to run the client and server on the
+	      //     same computer). InetAddress.getLocalHost() returns the Internet
+	      //     address of the local host.
+	      //  69 - the destination port number on the destination host.
+	      try {
+	    	  /*sendPacket = new DatagramPacket(msg, len,
+	                               InetAddress.getLocalHost(), sendPort);*/
+	    	  sendPacket = new DatagramPacket(msg, len,
+	        			InetAddress.getByName(ipAddress), sendPort);
+	    	  } catch (UnknownHostException e) {
+	           e.printStackTrace();
+	           System.exit(1);
+	        }
+	        	
+	        len = sendPacket.getLength();
+	        if (verboseMode) {
+	        	System.out.println("Client: sending packet.");
+	        	System.out.println("To host: " + sendPacket.getAddress());
+	        	System.out.println("Destination host port: " + sendPacket.getPort());
+	        	System.out.println("Length: " + len);
+	        	System.out.println("Containing: ");
+	        	for (j=0;j<len;j++) {
+	        		System.out.println("byte " + j + " " + msg[j]);
+	        	}
+	        } else {
+	        	System.out.println("Client: sending packet.");
+	        }
+	        
+	        // Form a String from the byte array, and print the string.
+	        String sending = new String(msg,0,len);
+	        System.out.println(sending);
+
+	        // Send the datagram packet to the server via the send/receive socket.
+
+	        try {
+	           sendReceiveSocket.send(sendPacket);
+	        } catch (IOException e) {
+	           e.printStackTrace();
+	           System.exit(1);
+	        }
+
+	        System.out.println("Client: Packet sent.");
+
+	        // Construct a DatagramPacket for receiving packets up
+	        // to 100 bytes long (the length of the byte array).
+
+	        data = new byte[100];
+	        receivePacket = new DatagramPacket(data, data.length);
+
+	        System.out.println("Client: Waiting for packet.");
+	        try {
+	           // Block until a datagram is received via sendReceiveSocket.
+	           sendReceiveSocket.receive(receivePacket);
+	        } catch(IOException e) {
+	           e.printStackTrace();
+	           System.exit(1);
+	        }
+
+	        // Process the received datagram.
+	        len = receivePacket.getLength();
+	        if (verboseMode) {
+	        	System.out.println("Client: Packet received:");
+	        	System.out.println("From host: " + receivePacket.getAddress());
+	        	System.out.println("Host port: " + receivePacket.getPort());
+	        	System.out.println("Length: " + len);
+	        	System.out.println("Containing: ");
+	        	for (j=0;j<len;j++) {
+	        		System.out.println("byte " + j + " " + data[j]);
+	        	}
+	        } else {
+	            System.out.println("Client: Packet received.");
+	        }
+	        
+	        System.out.println();      
 	   }
-	   
-	   //User chooses read or write request
-	   while (!(input.equals("1") || input.equals("2"))){
-		   System.out.println("\nEnter '1' for read or '2' write request: ");
-		   input = sc.nextLine();
-
-		   if (input.equals("1")) readWrite = (byte) 1;
-		   else if (input.equals("2")) readWrite = (byte) 2;
-		   else System.out.print(input + " is not 1 or 2.\n");
-	   }
-	   
-	   //INCOMPLETED, need to implement error detection for checking if file exists
-	   input = " ";
-	   String fileName = "";
-	   //while (found){ //keep asking for file until valid file is found in directory
-		   System.out.println("\nEnter the name of the file: ");
-		   fileName = sc.nextLine();
-		   //CHECK IF FILE EXISTS
-	   //}
-	   
-	   byte[] msg = new byte[100], // message we send
-             fn, // filename as an array of bytes
-             md, // mode as an array of bytes
-             data; // reply as array of bytes
-	   String mode; // filename and mode as Strings
-	   int j, len, sendPort;
-      
-      // In the assignment, students are told to send to 23, so just:
-      // sendPort = 23; 
-      // is needed.
-      // However, in the project, the following will be useful, except
-      // that test vs. normal will be entered by the user.
-	   
-      if (run==Mode.NORMAL) 
-         sendPort = 69;
-      else
-         sendPort = 23;
-      System.out.println("Client: creating packet.");
-         
-      // Prepare a DatagramPacket and send it via sendReceiveSocket
-      // to sendPort on the destination host (also on this machine).
-
-      // if i even (2,4,6,8,10), it's a read; otherwise a write
-      // (1,3,5,7,9) opcode for read is 01, and for write 02
-      // And #11 is invalid (opcode 07 here -- could be anything)
-
-      msg[0] = 0;
-      msg[1]=readWrite;
-
-      // convert to bytes
-      fn = fileName.getBytes();
-      
-      // and copy into the msg
-      System.arraycopy(fn,0,msg,2,fn.length);
-      // format is: source array, source index, dest array,
-      // dest index, # array elements to copy
-      // i.e. copy fn from 0 to fn.length to msg, starting at
-      // index 2
-        
-      // now add a 0 byte
-      msg[fn.length+2] = 0;
-
-      // now add "octet" (or "netascii")
-      mode = "octet";
-      // convert to bytes
-      md = mode.getBytes();
-      
-      // and copy into the msg
-      System.arraycopy(md,0,msg,fn.length+3,md.length);
-      
-      len = fn.length+md.length+4; // length of the message
-      // length of filename + length of mode + opcode (2) + two 0s (2)
-      // second 0 to be added next:
-
-      // end with another 0 byte 
-      msg[len-1] = 0;
-
-        // Construct a datagram packet that is to be sent to a specified port
-        // on a specified host.
-        // The arguments are:
-        //  msg - the message contained in the packet (the byte array)
-        //  the length we care about - k+1
-        //  InetAddress.getLocalHost() - the Internet address of the
-        //     destination host.
-        //     In this example, we want the destination to be the same as
-        //     the source (i.e., we want to run the client and server on the
-        //     same computer). InetAddress.getLocalHost() returns the Internet
-        //     address of the local host.
-        //  69 - the destination port number on the destination host.
-        try {
-           /*sendPacket = new DatagramPacket(msg, len,
-                               InetAddress.getLocalHost(), sendPort);*/
-        	sendPacket = new DatagramPacket(msg, len,
-        			InetAddress.getByName(ipAddress), sendPort);
-        } catch (UnknownHostException e) {
-           e.printStackTrace();
-           System.exit(1);
-        }
-        	
-        len = sendPacket.getLength();
-        if (verboseMode) {
-        	System.out.println("Client: sending packet.");
-        	System.out.println("To host: " + sendPacket.getAddress());
-        	System.out.println("Destination host port: " + sendPacket.getPort());
-        	System.out.println("Length: " + len);
-        	System.out.println("Containing: ");
-        	for (j=0;j<len;j++) {
-        		System.out.println("byte " + j + " " + msg[j]);
-        	}
-        } else {
-        	System.out.println("Client: sending packet.");
-        }
-        
-        // Form a String from the byte array, and print the string.
-        String sending = new String(msg,0,len);
-        System.out.println(sending);
-
-        // Send the datagram packet to the server via the send/receive socket.
-
-        try {
-           sendReceiveSocket.send(sendPacket);
-        } catch (IOException e) {
-           e.printStackTrace();
-           System.exit(1);
-        }
-
-        System.out.println("Client: Packet sent.");
-
-        // Construct a DatagramPacket for receiving packets up
-        // to 100 bytes long (the length of the byte array).
-
-        data = new byte[100];
-        receivePacket = new DatagramPacket(data, data.length);
-
-        System.out.println("Client: Waiting for packet.");
-        try {
-           // Block until a datagram is received via sendReceiveSocket.
-           sendReceiveSocket.receive(receivePacket);
-        } catch(IOException e) {
-           e.printStackTrace();
-           System.exit(1);
-        }
-
-        // Process the received datagram.
-        len = receivePacket.getLength();
-        if (verboseMode) {
-        	System.out.println("Client: Packet received:");
-        	System.out.println("From host: " + receivePacket.getAddress());
-        	System.out.println("Host port: " + receivePacket.getPort());
-        	System.out.println("Length: " + len);
-        	System.out.println("Containing: ");
-        	for (j=0;j<len;j++) {
-        		System.out.println("byte " + j + " " + data[j]);
-        	}
-        } else {
-            System.out.println("Client: Packet received.");
-        }
-        
-        System.out.println();      
-
-      // We're finished, so close the socket.
-      sendReceiveSocket.close();
+	   System.out.println("Client is off");
+	   // We're finished, so close the socket.
+	   sendReceiveSocket.close();
    }
    
    /**
