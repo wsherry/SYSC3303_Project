276ce49f29d4853d98a7361243765ba25de1aa59
AlanSYSC <50414515+AlanSYSC@users.noreply.github.com>
2019-05-18 13:54:04 -0400

    Shut down v2 (#2)
    * Shutdown complete
    
    * Re-implemented shut down to work with UI

--- src/TFTPClientConnectionThread.java
+++ src/TFTPClientConnectionThread.java
@@ -1,13 +1,12 @@
 import java.io.*;
 import java.net.*;
 
+
 public class TFTPClientConnectionThread implements Runnable {
 
-	private DatagramSocket sendSocket;
+	private DatagramSocket receiveSocket;
 	private DatagramPacket receivePacket;
-	private DatagramPacket sendPacket;
 	private boolean verboseMode = false; //false for quiet and true for verbose
-
 	
 	private Request request;
 	// responses for valid requests
@@ -16,72 +15,206 @@ public class TFTPClientConnectionThread implements Runnable {
 	
 	String fileNameToWrite;
 
-	public TFTPClientConnectionThread(Request request, DatagramPacket receivePacket, boolean verboseMode) {
+	public TFTPClientConnectionThread(boolean verboseMode) {
 		try {
-			sendSocket = new DatagramSocket();
-		} catch (SocketException se) {
-			se.printStackTrace();
-			System.exit(0);
-		}
+	         // Construct a datagram socket and bind it to port 69
+	         // on the local host machine. This socket will be used to
+	         // receive UDP Datagram packets.
+	         receiveSocket = new DatagramSocket(69);
+	      } catch (SocketException se) {
+	         se.printStackTrace();
+	         System.exit(1);
+	      }
 
-		this.receivePacket = receivePacket;
-		this.request = request;
 		this.verboseMode = verboseMode;
+	}
+	
+	/**
+	 * Closes the receive socket
+	 */
+	void closeSocket() {
+		receiveSocket.close();
 	}
 
 	public void run() {
-	    byte[] response = new byte[4];
-		// Create a response.
-        if (request == Request.READ) { // for Read it's 0301
-           response = readResp;
-        } else if (request == Request.WRITE) { // for Write it's 0400
-           response = writeResp;
-        }
+		int len, j=0, k=0;
 
-		send(response);
+		while(true) { // loop forever
+			byte[] data = new byte[100];
+	        receivePacket = new DatagramPacket(data, data.length);
+
+	        System.out.println("------------------------------------------------------");
+	        System.out.println("Type 'quit' to shutdown.");
+	        try {
+				System.out.println("Server (" + InetAddress.getLocalHost().getHostAddress() + ") : Waiting for packet.");
+			} catch (UnknownHostException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+	        
+	        // Block until a datagram packet is received from receiveSocket.
+	        try {
+	        	receiveSocket.receive(receivePacket);
+	        } catch (SocketException exception) { //the socket has been closed for server shut down
+   			  	System.out.println("Server is off");
+	        	break;
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	            System.exit(1);
+	        }
+
+	        // Process the received datagram.
+	        len = receivePacket.getLength();
+	        if (verboseMode) {
+	        	System.out.println("Server: Packet received:");
+	        	System.out.println("From host: " + receivePacket.getAddress());
+	        	System.out.println("Host port: " + receivePacket.getPort());
+	        	System.out.println("Length: " + len);
+	        	System.out.println("Containing: " );
+	        	// print the bytes
+	        	for (j=0;j<len;j++) {
+	        		System.out.println("byte " + j + " " + data[j]);
+	        	} 
+	        } else {
+	            System.out.println("Server: Packet received.");
+	        }
+	         
+	         // Form a String from the byte array.
+	         String received = new String(data,0,len);
+	         System.out.println(received);
+
+	         // If it's a read, send back DATA (03) block 1
+	         // If it's a write, send back ACK (04) block 0
+	         // Otherwise, ignore it
+	         if (data[0]!=0) request = Request.ERROR; // bad
+	         else if (data[1]==1) request = Request.READ; // could be read
+	         else if (data[1]==2) request = Request.WRITE; // could be write
+	         else request = Request.ERROR; // bad
+
+	         if (request!=Request.ERROR) { // check for filename
+	             // search for next all 0 byte
+	             for(j=2;j<len;j++) {
+	                 if (data[j] == 0) break;
+	            }
+	            if (j==len) request=Request.ERROR; // didn't find a 0 byte
+	            if (j==2) request=Request.ERROR; // filename is 0 bytes long
+	            // otherwise, extract filename
+	         }
+	 
+	         if(request!=Request.ERROR) { // check for mode
+	             // search for next all 0 byte
+	             for(k=j+1;k<len;k++) { 
+	                 if (data[k] == 0) break;
+	            }
+	            if (k==len) request=Request.ERROR; // didn't find a 0 byte
+	            if (k==j+1) request=Request.ERROR; // mode is 0 bytes long
+	         }
+	         
+	         if(k!=len-1) request=Request.ERROR; // other stuff at end of packet        
+
+	         // Create a response.
+	         if (request == Request.READ) { 
+	        	 //create new thread for sending data
+	        	 Runnable readReqThread = new TFTPsendThread(request, receivePacket, verboseMode);
+	             new Thread(readReqThread).start();
+	             
+	         } else if (request == Request.WRITE) {
+	        	 //create new thread for sending data
+	        	 Runnable writeReqThread = new TFTPsendThread(request, receivePacket, verboseMode);
+	             new Thread(writeReqThread).start();
+	         }
+	         
+	         if (request == Request.ERROR) { // it was invalid, close socket on port 69 (so things work properly next time) and quit
+	            receiveSocket.close();
+	            try {
+					throw new Exception("Not yet implemented");
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	         }
+		} // end of loop
 	}
+	
+	public class TFTPsendThread implements Runnable {
 
-	private void send(byte[] response) {
-        sendPacket = new DatagramPacket(response, response.length,
-                              receivePacket.getAddress(), receivePacket.getPort());
-        
-        int len = sendPacket.getLength();
-        if (verboseMode) {
-        	System.out.println("TFTPClientConnectionThread: Sending packet:");
-        	System.out.println("To host: " + sendPacket.getAddress());
-        	System.out.println("Destination host port: " + sendPacket.getPort());
-        	System.out.println("Length: " + len);
-        	System.out.println("Containing: ");
-        	for (int j=0; j<len; j++) {
-        		System.out.println("byte " + j + " " + response[j]);
-        	}
-        } else {
-            System.out.println("Server: Packet sent.");
-        }
+		private DatagramSocket sendSocket;
+		private DatagramPacket receivePacket;
+		private DatagramPacket sendPacket;
+		private boolean verboseMode = false; //false for quiet and true for verbose
 
-        // Send the datagram packet to the client via a new socket.
 
-        try {
-           // Construct a new datagram socket and bind it to any port
-           // on the local host machine. This socket will be used to
-           // send UDP Datagram packets.
-           sendSocket = new DatagramSocket();
-        } catch (SocketException se) {
-           se.printStackTrace();
-           System.exit(1);
-        }
+		private Request request;
+		// responses for valid requests
+		String fileNameToWrite;
 
-        try {
-           sendSocket.send(sendPacket);
-        } catch (IOException e) {
-           e.printStackTrace();
-           System.exit(1);
-        }
+		public TFTPsendThread(Request request, DatagramPacket receivePacket, boolean verboseMode) {
+			try {
+				sendSocket = new DatagramSocket();
+			} catch (SocketException se) {
+				se.printStackTrace();
+				System.exit(0);
+			}
 
-        System.out.println("TFTPClientConnectionThread: packet sent using port " + sendSocket.getLocalPort());
-        System.out.println();
+			this.receivePacket = receivePacket;
+			this.request = request;
+			this.verboseMode = verboseMode;
+		}
 
-        // We're finished with this socket, so close it.
-        sendSocket.close();		
+		public void run() {
+			byte[] response = new byte[4];
+			// Create a response.
+			if (request == Request.READ) { // for Read it's 0301
+				response = readResp;
+			} else if (request == Request.WRITE) { // for Write it's 0400
+				response = writeResp;
+			}
+
+			send(response);
+		}
+
+		private void send(byte[] response) {
+			sendPacket = new DatagramPacket(response, response.length,
+					receivePacket.getAddress(), receivePacket.getPort());
+
+			int len = sendPacket.getLength();
+			if (verboseMode) {
+				System.out.println("TFTPClientConnectionThread: Sending packet:");
+				System.out.println("To host: " + sendPacket.getAddress());
+				System.out.println("Destination host port: " + sendPacket.getPort());
+				System.out.println("Length: " + len);
+				System.out.println("Containing: ");
+				for (int j=0; j<len; j++) {
+					System.out.println("byte " + j + " " + response[j]);
+				}
+			} else {
+				System.out.println("Server: Packet sent.");
+			}
+
+			// Send the datagram packet to the client via a new socket.
+
+			try {
+				// Construct a new datagram socket and bind it to any port
+				// on the local host machine. This socket will be used to
+				// send UDP Datagram packets.
+				sendSocket = new DatagramSocket();
+			} catch (SocketException se) {
+				se.printStackTrace();
+				System.exit(1);
+			}
+
+			try {
+				sendSocket.send(sendPacket);
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+
+			System.out.println("TFTPClientConnectionThread: packet sent using port " + sendSocket.getLocalPort());
+			System.out.println();
+
+			// We're finished with this socket, so close it.
+			sendSocket.close();		
+		}
 	}
 }
--- src/TFTPServer.java
+++ src/TFTPServer.java
@@ -4,20 +4,16 @@
 // sends back the appropriate response without any actual file transfer.
 // One socket (69) is used to receive (it stays open) and another for each response. 
 
-import java.io.*;
-import java.net.*;
 import java.util.Scanner;
 
 
 public class TFTPServer {
    // UDP datagram packets and sockets used to send / receive
-   private DatagramPacket receivePacket;
-   private DatagramSocket receiveSocket;
    private static boolean verboseMode = false; //false for quiet and true for verbose
    private static String serverDirectory = "";
    private static boolean fisnishedRequest = false;
    
-   public TFTPServer()
+   /*public TFTPServer()
    {
       try {
          // Construct a datagram socket and bind it to port 69
@@ -28,107 +24,43 @@ public class TFTPServer {
          se.printStackTrace();
          System.exit(1);
       }
-   }
+   }*/
 
-   public void receiveAndSendTFTP() throws Exception
+   /**
+    * Launches the thread for receiving data
+    * and continuously runs the server's GUI.
+    * 
+    */
+   public void receiveAndSendTFTP()
    {
-      byte[] data = new byte[4];     
-      Request req; // READ, WRITE or ERROR
 	  Scanner sc = new Scanner (System.in);
-	  String input = "";
-	   
-      int len, j=0, k=0;
+	  String input;
+	  
+	  //create ne thread for receiving
+	  TFTPClientConnectionThread recieveThread = new TFTPClientConnectionThread(verboseMode);
+      new Thread(recieveThread).start();
 
-      for(;;) { // loop forever
+      while(true) { // loop forever
     	  input = "";
-    	  //After a request has been completed the user gets prompted
+    	  
+    	  //Server exits loop only when server receives 'quit' input
+     	  if(sc.nextLine().toUpperCase().equals("QUIT")) {
+   			  System.out.println("Server is shutting down.");
+     		  recieveThread.closeSocket();
+     		  break;
+     	  }     	  
+     	  //After a request has been completed the user gets prompted
     	  //to enter the configuration "menu" again
     	  //TO BE IMPLEMENTED AFTER FILE TRANSFER IMPLEMENTATION IS COMPLETED
-    	  if (fisnishedRequest) { //fisnishedRequest should be true after a file has been fully read or written
-    		  System.out.println("Enter 1 to change configerations or nothing to leave configs unchanged: ");
-    		  while (!(input.equals("1") || input.equals(""))){
-    			  input = sc.nextLine();
-    			  if (input.equals("1")) configServer();
-    		  }
-    		  fisnishedRequest = false; //set fisnishedRequest to false so the user only gets prompt after a request finishes
-    	  }
-    	  
-         // Construct a DatagramPacket for receiving packets up
-         // to 100 bytes long (the length of the byte array).
-         
-         data = new byte[100];
-         receivePacket = new DatagramPacket(data, data.length);
-
-         System.out.println("------------------------------------------------------");
-         System.out.println("Server (" + InetAddress.getLocalHost().getHostAddress() + ") : Waiting for packet.");
-         // Block until a datagram packet is received from receiveSocket.
-         try {
-            receiveSocket.receive(receivePacket);
-         } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(1);
-         }
-
-         // Process the received datagram.
-         len = receivePacket.getLength();
-         if (verboseMode) {
-        	 System.out.println("Server: Packet received:");
-        	 System.out.println("From host: " + receivePacket.getAddress());
-        	 System.out.println("Host port: " + receivePacket.getPort());
-        	 System.out.println("Length: " + len);
-        	 System.out.println("Containing: " );        	 
-         } else {
-             System.out.println("Server: Packet received.");
-         }
-         
-         
-         // print the bytes
-         for (j=0;j<len;j++) {
-            System.out.println("byte " + j + " " + data[j]);
-         }
-
-         // Form a String from the byte array.
-         String received = new String(data,0,len);
-         System.out.println(received);
-
-         // If it's a read, send back DATA (03) block 1
-         // If it's a write, send back ACK (04) block 0
-         // Otherwise, ignore it
-         if (data[0]!=0) req = Request.ERROR; // bad
-         else if (data[1]==1) req = Request.READ; // could be read
-         else if (data[1]==2) req = Request.WRITE; // could be write
-         else req = Request.ERROR; // bad
-
-         if (req!=Request.ERROR) { // check for filename
-             // search for next all 0 byte
-             for(j=2;j<len;j++) {
-                 if (data[j] == 0) break;
-            }
-            if (j==len) req=Request.ERROR; // didn't find a 0 byte
-            if (j==2) req=Request.ERROR; // filename is 0 bytes long
-            // otherwise, extract filename
-         }
- 
-         if(req!=Request.ERROR) { // check for mode
-             // search for next all 0 byte
-             for(k=j+1;k<len;k++) { 
-                 if (data[k] == 0) break;
-            }
-            if (k==len) req=Request.ERROR; // didn't find a 0 byte
-            if (k==j+1) req=Request.ERROR; // mode is 0 bytes long
-         }
-         
-         if(k!=len-1) req=Request.ERROR; // other stuff at end of packet        
-         
-         if (req == Request.ERROR) { // it was invalid, close socket on port 69 (so things work properly next time) and quit
-            receiveSocket.close();
-            throw new Exception("Not yet implemented");
-         }
-
-      	 // Create a new client connection thread for each connection with the client.
-         Runnable newClient = new TFTPClientConnectionThread(req, receivePacket, verboseMode);
-         new Thread(newClient).start();
-      } // end of loop
+     	  /*if (fisnishedRequest) { //fisnishedRequest should be true after a file has been fully read or written
+     		  System.out.println("Enter 1 to change configurations or nothing to leave configs unchanged: ");
+     		  while (!(input.equals("1") || input.equals(""))){
+     			  input = sc.nextLine();
+     			  if (input.equals("1")) configServer();
+     		  }
+     		  fisnishedRequest = false; //set fisnishedRequest to false so the user only gets prompt after a request finishes
+     	  }*/
+      }
    }
    
    /**
@@ -165,7 +97,6 @@ public class TFTPServer {
 			   System.out.println("Server directory is now: " + serverDirectory);
 		   }
 	   }
-	   
 	   System.out.println("\n------------------------------------------------------\nConfigerations are now set up.");
    }
 
